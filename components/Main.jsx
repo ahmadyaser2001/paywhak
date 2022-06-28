01b3efe400e69194210124f73fb9870755da3d7f
Yaser <ahmadyaserazizi2001@gmail.com>
2022-06-27 18:59:00 -0700

    single-page

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,19 +10,21 @@ const Main = () => {
       <div
         class="container flex flex-col-reverse items-center px-6 mx-auto   space-y-0 md:space-y-0 md:flex-row"
       >
-        <div class="md:w-1/2">
+        <div class="mt-10 md:w-1/2">
           <Image src={p4} alt="" />
         </div>
-        <div class="flex flex-col mb-32 space-y-12 md:w-1/2 mt-20">
+        <div class="flex flex-col  space-y-12 md:w-1/2 mt-20">
           <h1
             className="text-white max-w-md text-4xl font-bold text-center md:text-5xl md:text-left"
           >
-		<p className='text-2xl text-black opacity-0'>	Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dolores natus sit, est vero libero</p> 
+		<p className='text-2xl text-black opacity-0'> 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dolores natus sit, est vero libero.
+	 </p> 
             Bring everyone together to build better products
           </h1>
           <p class="max-w-sm text-center text-white md:text-left ">
             Manage makes it simple for software teams to plan day-to-day tasks
             while keeping the larger team goals in view.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dolores natus sit, est vero libero.
           </p>
           <div class="flex justify-center md:justify-start">
 			  <div class="inline-flex rounded-md shadow-sm">
